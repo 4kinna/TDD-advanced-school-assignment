ae3c45595a57b1cdbf48b46daeb99675f2460d9b
Annika <a.staalhandske@gmail.com>
2023-01-23 11:38:04 +0100

    init

---
+++ src/main/java/com/example/tddadvancedschoolassignment/TddAdvancedSchoolAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.tddadvancedschoolassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TddAdvancedSchoolAssignmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TddAdvancedSchoolAssignmentApplication.class, args);
+    }
+
+}
